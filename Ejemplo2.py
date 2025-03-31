70d060e43550b0deab202202e882e60335b71977
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-03-31 12:35:43 -0600

    limpieza

--- Ejemplo2.py
+++
@@ -1,3 +0,0 @@
-number = 31
-print("El numero que posee la variable es", number)
-print("el tipo de la variable es ", type(number))
--- ejemplo3.py
+++
@@ -1,3 +0,0 @@
-#leer el nombre de una persona y saludarla 
-name = input("¿Cual es tu nombre?")
-print("HOLA,", name, "mucho gusto")
